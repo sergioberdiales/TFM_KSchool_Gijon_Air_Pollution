c178d5cccb785be6fa369146b42be7877bbce30d
sergioberdiales <sergioberdiales@gmail.com>
2018-11-14 21:12:39 +0100

    Modeling PM10 monthly ARIMA

--- notes.R
+++ notes.R
@@ -44,3 +44,82 @@ nas_per <- air_data_2 %>%
   group_by(station_alias, year(date_time_utc)) %>%
   select(everything()) %>%  
   summarise_all(funs(round(sum(is.na(.))/n(), 2)))
+
+
+# Voy a hacer la prediccion mensual de pm10 con otro paquete de r. Pego aqui todo el codigo utilizado hasta ahora
+
+And we plot it.
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+plot(pm10_month_ts)
+
+```
+
+
+We remove the trend with the "diff" and plot it.
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+plot(diff(pm10_month_ts))
+
+```
+
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+acf2(pm10_month_ts)
+
+```
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+sarima(pm10_month_ts, 0, 0, 1)
+
+```
+
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+sarima(pm10_month_ts, 1, 0, 0)
+
+```
+
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+sarima(pm10_month_ts, 1, 0, 1)
+
+```
+
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+sarima(pm10_month_ts, 1, 1, 1)
+
+```
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+sarima(pm10_month_ts, p = 1, d = 1, q = 1, P = 0, D = 1, Q = 1, S = 12)
+
+```
+
+As we saw before there are two clearly different periods in the data. We are going to use now only the second period.
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+
+sarima(pm10_2nd_ts, p = 1, d = 1, q = 1, P = 0, D = 1, Q = 1, S = 12)
+
+```
+
+The results are significantly better.
+
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+
+sarima(pm10_2nd_ts, p = 1, d = 1, q = 1, P = 1, D = 1, Q = 1, S = 12)
+
+```
+
+
+Predicting 12 months
+
+```{r warning = FALSE, message= FALSE, out.width= '\\textwidth'}
+
+
+sarima.for(pm10_2nd_ts, n.ahead = 12, 1, 1, 1, 1, 1, 1, 12)
+
+```
